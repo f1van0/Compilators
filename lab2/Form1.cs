7e6fceff9844f41c2c9081aab7de5213a888c231
Optifun <frdima19@gmail.com>
2020-03-22 15:35:50 +0700

    Lab 2
    Программа генерирующая текст на "естесственном" языке
    Здесь добавлены классы:
    - Слово
    - Терминальное слово
    - Нетерминальное слово
    - Существительное(Noun)
    - Глагол(Verb)
    - Прилагательное(Adjective)
    - Союз(Union)
    - Предлог(PreText)
    - Generator

---
+++ lab2/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using lab2.Producer;
+
+namespace lab2
+	{
+	public partial class Form1 : Form
+		{
+		public Form1 ()
+			{
+			InitializeComponent();
+			textBox1.Text = "Подлежащее Сказуемое\r\nПодлежащее Сказуемое Дополнение\r\nСказуемое Подлежащее Дополнение\r\nСказуемое Подлежащее\r\nСказуемое Дополнение";
+			}
+
+		private void label2_Click (object sender, EventArgs e)
+			{
+			}
+
+		private void button1_Click (object sender, EventArgs e)
+			{
+			textBox2.Text = "";
+			string[] patterns = textBox1.Text.Split("\n\r".ToArray(), StringSplitOptions.RemoveEmptyEntries);
+			foreach ( var pattern in patterns )
+				{
+				try
+					{
+					string Sentence = Generator.Generate(pattern);
+					textBox2.Text += Sentence + "\r\n";
+					}
+				catch ( Exception ex)
+					{
+					textBox2.Text += ex.Message;
+					break;
+					}
+				}
+			}
+		}
+	}
---
+++ lab2/Producer/Noun.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab2.Producer
+	{
+	/// <summary>
+	/// Существительное
+	/// </summary>
+	class Noun : TerminateWord
+		{
+		protected static string[] Finalizes;
+		public WordGender Gender { get; protected set; }
+		static Noun ()
+			{
+			//Gender
+			// He, She, It, They
+			Finalizes = new string[4] { "", "а", "ие", "ы" };
+			}
+
+		public Noun (string wordBase, WordGender gen) : base(wordBase, Terminate.Существительное)
+			{
+			Gender = gen;
+			}
+
+		public string GetNoun ()
+			{
+			return WordBase;
+			}
+		}
+
+	}
